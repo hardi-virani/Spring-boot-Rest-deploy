5def8fdb84fe525798249235b0f595ca6140c018
Hardi <hardivirani2003@gmail.com>
2026-06-20 18:21:20 -0400

    vid : 85 - Connecting React & Spring

--- src/main/java/com/star/spring_boot_rest/JobRestController.java
+++ src/main/java/com/star/spring_boot_rest/JobRestController.java
@@ -4,6 +4,8 @@ import com.star.spring_boot_rest.model.JobPost;
 import com.star.spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class JobRestController {
 
     @Autowired
@@ -20,6 +23,7 @@ public class JobRestController {
     @GetMapping("jobPosts")
 //    @ResponseBody  //Instead of this we have written @RestController
     public List<JobPost> getAllJobs() {
+        System.out.println("Endpoint Hit");
             return service.getAllJobs();
     }
 
