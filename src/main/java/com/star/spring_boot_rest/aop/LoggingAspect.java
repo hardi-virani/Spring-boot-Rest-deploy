c518bd97f4fe1bca9c447ef435ddbc586fed40ad
Hardi <hardivirani2003@gmail.com>
2026-06-27 14:26:55 -0400

    vid : 129 : Before Advice

--- src/main/java/com/star/spring_boot_rest/aop/LoggingAspect.java
+++ src/main/java/com/star/spring_boot_rest/aop/LoggingAspect.java
@@ -1,14 +1,22 @@
 package com.star.spring_boot_rest.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    public void logMethodCall() {
+    // return type, class name.method-name(args)
 
+    @Before("execution(* com.star.spring_boot_rest.service.JobService.*(..))")
+    public void logMethodCall() {
         LOGGER.info("Method called");
 
     }
